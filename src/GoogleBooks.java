bc95674fb631651b6d6e95726bd600e31cccb3b7
Jakub Brzostek <jakubbrzostek@MacBook-Pro-Jakub.local>
2020-02-22 00:20:28 +0100

    Inititial commit

---
+++ src/GoogleBooks.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class GoogleBooks {
+
+    public static void main(String[] args) {
+
+        Map<String,String> dataSets = new HashMap<String,String>() {
+
+        };
+    }
+}
